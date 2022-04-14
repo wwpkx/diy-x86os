272147fd98734c9222045b748c7d981a952aa5fd
lishutong <527676163@qq.com>
2022-04-14 18:02:18 +0800

    工程拆分

---
+++ code/c12.15的副本/source/kernel/include/fs/file.h
@@ -0,0 +1,18 @@
+/**
+ * 文件系统相关接口的实现
+ *
+ * 创建时间：2021年8月5日
+ * 作者：李述铜
+ * 联系邮箱: 527676163@qq.com
+ */
+#ifndef FILE_H
+#define FILE_H
+
+int sys_open(const char *name, int flags, ...);
+int sys_read(int file, char *ptr, int len);
+int sys_write(int file, char *ptr, int len);
+int sys_lseek(int file, int ptr, int dir);
+int sys_close(int file);
+
+#endif // FILE_H
+
